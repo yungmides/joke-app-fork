84e10a16f11159573def3fad9aa13fb81cafef19
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-09-22 10:40:25 +0200

    gnagnagna

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000, () => {
     console.log(`API Version : ${process.env.npm_package_version}`);
   });
